524cf82326b09fb54fc9f0be9fcf4c0a070b408c
Robson <robccorreia@gmail.com>
2024-03-28 15:38:15 -0300

    feat: add dynamic pages and new query get_page_by_slug

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,40 @@
+import client from 'graphql/client'
+import { GET_PAGE_BY_SLUG, GET_PAGES } from 'graphql/queries'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import PageTemplate, { type PageTemplateProps } from 'templates/Pages'
+
+export default function Page({ heading, body }: PageTemplateProps) {
+  const router = useRouter()
+  // retorna um loading, qualquer coisa enquanto está sendo criado
+  if (router.isFallback) return null
+  return <PageTemplate heading={heading} body={body} />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+  return { paths, fallback: true }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (!page) return { notFound: true }
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  }
+}
+
+//getStaticPaths => serve para gerar as urls em build time /about /trip/petropolis
+//getStaticProps => serve para buscar os dados da página (props) em build time - estático
+//getServerSideProps => serve para buscar os dados da página em runtime - toda requisição (bom para SEO) (blundle fica so no server)
+//getInitialProps => serve para buscar os dados da página (props) - runtime- toda requisiçao fica no client - hydrate
