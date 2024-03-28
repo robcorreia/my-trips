7b1e2e36fef16f05f24f2a3e44bb5bedae6fdb97
Robson <robccorreia@gmail.com>
2024-03-28 00:36:48 -0300

    feat: add query GET_PAGES

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,20 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const AboutPage = () => {
+export default function AboutPage() {
   return <AboutTemplate></AboutTemplate>
 }
 
-export default AboutPage
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+  console.log(pages)
+  return {
+    props: {}
+  }
+}
+
+//getStaticPaths => serve para gerar as urls em build time /about /trip/petropolis
+//getStaticProps => serve para buscar os dados da página (props) em build time - estático
+//getServerSideProps => serve para buscar os dados da página em runtime - toda requisição (bom para SEO) (blundle fica so no server)
+//getInitialProps => serve para buscar os dados da página (props) - runtime- toda requisiçao fica no client - hydrate
