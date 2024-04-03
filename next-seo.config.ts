3124edfa75f6c63b79d22d760e1a191463ab9a3a
Robson <robccorreia@gmail.com>
2024-04-03 14:59:38 -0300

    feat: add SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://my-trips.robsoncorreia.com.br',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@robcorreia',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
 
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config'
+
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -18,11 +20,7 @@ function App({ Component, pageProps }: AppProps) {
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project to show my favorite spots in the world."
-        />
+        <DefaultSeo {...SEO} />
       </Head>
       <GlobalStyles />
       <NextNProgress
